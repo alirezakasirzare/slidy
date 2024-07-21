54d4b14a90649728424c7d1094b13549fcccfa76
Alireza Kasirzare <alireza.kasirzare@gmail.com>
2024-07-22 00:30:29 +0330

    fix type

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,9 @@
 import type { MetaFunction } from "@remix-run/node";
 import { Sidebar } from "~/components/common/sidebar/sidebar";
+import {
+  RowElement,
+  RowElementConfig,
+} from "~/components/elements/row-element";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,11 +13,14 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const config: RowElementConfig = {};
   return (
     <div className="flex min-h-screen">
       <Sidebar />
       <main className="grow px-10 py-6">
-        <div className="border h-full rounded-xl p-4">test</div>
+        <div className="border h-full rounded-xl p-4">
+          <RowElement />
+        </div>
       </main>
     </div>
   );
