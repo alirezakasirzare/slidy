46349307a35bf61bdda8f3b6e8b8cece1c305743
Alireza Kasirzare <alireza.kasirzare@gmail.com>
2024-07-22 00:00:31 +0330

    fix types

--- app/components/elements/row-element.tsx
+++ app/components/elements/row-element.tsx
@@ -1,55 +1,57 @@
 import { BoxIcon } from "lucide-react";
-import { Element, ElementOption } from "~/types";
+import { Element } from "~/types";
 
-const justifyOption: ElementOption<"justify"> = {
-  text: "Justify",
-  name: "justify",
-  value: [
-    {
-      text: "Start",
-      value: "start",
-    },
-    {
-      text: "Center",
-      value: "center",
-    },
-    {
-      text: "End",
-      value: "end",
-    },
-  ],
-};
-
-const alignOption: ElementOption<"align"> = {
-  text: "Align",
-  name: "align",
-  value: [
-    {
-      text: "Start",
-      value: "start",
-    },
-    {
-      text: "Center",
-      value: "center",
-    },
-    {
-      text: "End",
-      value: "end",
-    },
-  ],
-};
-
-const gapOption: ElementOption<"gap"> = {
-  text: "Gap",
-  name: "gap",
-  value: "number",
-};
-
-export const rowElementConfig: Element<"row", "justify" | "align" | "gap"> = {
+export const rowElementConfig: Element<
+  "row",
+  "justify" | "align" | "gap",
+  "start" | "center" | "end"
+> = {
   text: "Row",
   name: "row",
   icon: <BoxIcon />,
-  options: [justifyOption, alignOption, gapOption],
+  options: [
+    {
+      text: "Justify",
+      name: "justify",
+      value: [
+        {
+          text: "Start",
+          value: "start",
+        },
+        {
+          text: "Center",
+          value: "center",
+        },
+        {
+          text: "End",
+          value: "end",
+        },
+      ],
+    },
+    {
+      text: "Align",
+      name: "align",
+      value: [
+        {
+          text: "Start",
+          value: "start",
+        },
+        {
+          text: "Center",
+          value: "center",
+        },
+        {
+          text: "End",
+          value: "end",
+        },
+      ],
+    },
+    {
+      text: "Gap",
+      name: "gap",
+      value: "number",
+    },
+  ],
 };
 
 export const RowElement = () => {
--- app/types/index.ts
+++ app/types/index.ts
@@ -1,14 +1,14 @@
-export type Element<T, J> = {
+export type Element<T, J, V> = {
   text: string;
   name: T;
   icon: React.ReactElement;
-  options: ElementOption<J>[];
+  options: ElementOption<J, V>[];
 };
 
-export type ElementOption<T> = {
+export type ElementOption<T, J> = {
   text: string;
   name: T;
-  value: ElementOptionValue;
+  value: ElementOptionValue<J>;
 };
 
-export type ElementOptionValue = "number" | { text: string; value: string }[];
+export type ElementOptionValue<T> = "number" | { text: string; value: T }[];
