3f25dafc4e71a43f9461413a589a5c329be9229a
Alireza Kasirzare <alireza.kasirzare@gmail.com>
2024-07-21 22:46:10 +0330

    add sidebar

---
+++ app/components/common/sidebar.tsx
@@ -0,0 +1,3 @@
+export const Sidebar = () => {
+  return <aside className="w-[300px] bg-accent shrink-0 p-5">side bar</aside>;
+};
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Sidebar } from "~/components/common/sidebar";
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,5 +9,12 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  return <div>Home</div>;
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <main className="grow px-10 py-6">
+        <div className="border h-full rounded-xl p-4">test</div>
+      </main>
+    </div>
+  );
 }
